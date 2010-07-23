77114fcebe3a989b7f37b197485ae40142d33694
Michael White <mwhite@mwhite.info>
2010-07-23 20:02:51 +0000

    more refactoring

--- atwl/ATWL.php
+++ atwl/ATWL.php
@@ -1,6 +1,27 @@
 <?php
 
-$dir = dirname(__FILE__) . '/';
+$wgExtensionFunctions[] = 'wfATWLSetup';
 
-$wgAutoloadClasses['SpecialATWL'] = $dir . 'SpecialATWL.php';
+if( !defined( 'MEDIAWIKI' ) ) {
+	die("Not an entry point.\n");
+}
+
+$dir = dirname(__FILE__) . '/';
 $wgSpecialPages['ATWL'] = 'SpecialATWL';
+$wgAutoloadClasses['SpecialATWL'] = $dir . 'SpecialATWL.php';
+
+
+function wfATWLSetup() {
+	global $wgAutoloadClasses, $wgHooks;
+	
+	$dir = dirname(__FILE__) . '/';
+	
+
+	$wgAutoloadClasses['ATWQueryTree'] = $dir . 'ATWQueryTree.php';
+	$wgAutoloadClasses['ATWQueryNode'] = $dir . 'ATWQueryTree.php';
+	$wgAutoloadClasses['ATWKeywordData'] = $dir . 'ATWQueryTree.php';
+	$wgAutoloadClasses['ATWKeyword'] = $dir . 'ATWQueryTree.php';
+	$wgAutoloadClasses['ATWStore'] = $dir . 'ATWStore.php';
+	
+	
+}
