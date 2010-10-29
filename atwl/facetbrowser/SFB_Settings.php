afc156b86803fbf4b9787897d074430b60690c1e
Michael White <mwhite@mwhite.info>
2010-10-29 05:07:34 +0000

    change default  to true

--- atwl/facetbrowser/SFB_Settings.php
+++ atwl/facetbrowser/SFB_Settings.php
@@ -9,7 +9,7 @@ $atwgShowFacets = true;
 $atwgAlwaysShowFacets = false;  // not yet implemented (simple)
 
 // if this is true, then [[property::+]] is added to the query for every ?property
-//$atwgPrintoutsMustExist = true;
+$atwgPrintoutsMustExist = true;
 
 // if this is true, then a keyword intrepreted as [[property::value]] adds a ?Property statement
 //$atwgPrintoutConstrainedProperties = true;
